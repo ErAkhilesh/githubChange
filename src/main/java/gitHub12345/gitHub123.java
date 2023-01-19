63c0420d79c03c35f37c2ee1306d2bcd443f5171
gakhi <gakhi@DESKTOP-DB9ED7V>
2023-01-19 13:02:33 +0530

    pankaj is my friend

---
+++ src/main/java/gitHub12345/gitHub123.java
@@ -0,0 +1,11 @@
+package gitHub12345;
+
+import org.testng.annotations.Test;
+
+public class gitHub123 {
+	@Test
+	public void delet() {
+		System.out.println("Today is ");
+	}
+	}
+
---
+++ src/main/java/gitHub12345/gitHubPractic678.java
@@ -0,0 +1,11 @@
+package gitHub12345;
+
+import org.testng.annotations.Test;
+
+public class gitHubPractic678 {
+	@Test
+	public void delet() {
+		System.out.println("Today is ");
+	}
+	}
+
