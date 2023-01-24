9103ccb47b773348218f642249f885e1f0ec9145
gakhi <gakhi@DESKTOP-DB9ED7V>
2023-01-24 14:49:33 +0530

    h

---
+++ src/main/java/gitHubPractic/PracticClass1.java
@@ -0,0 +1,11 @@
+package gitHubPractic;
+
+import org.testng.annotations.Test;
+
+public class PracticClass1 {
+@Test
+public void update()
+{
+	System.out.println("update");
+}
+}
---
+++ src/main/java/gitHubPractic/gitHubPractice2.java
@@ -0,0 +1,10 @@
+package gitHubPractic;
+
+import org.testng.annotations.Test;
+
+public class gitHubPractice2 {
+@Test
+public void delete() {
+	System.out.println("Sussessfully delete");
+}
+}
